e716549c7cc7757e2db2b053d8ea29a1b893eb43
Siobhantt <luisa.cardozo@iesnervion.es>
2023-05-16 23:18:03 +0200

    Ahora esta mas bonito.

--- Excepciones/src/ej2/NegativeMinuteException.java
+++ Excepciones/src/ej2/NegativeMinuteException.java
@@ -2,6 +2,6 @@ package ej2;
 
 public class NegativeMinuteException extends Exception {
 public String toString() {
-	return "No se puede colocar un digito menor a cero";
+	return "No se puede colocar un minuto menor a cero";
 }
 }
--- Excepciones/src/ej2/NegativeSecondException.java
+++ Excepciones/src/ej2/NegativeSecondException.java
@@ -2,6 +2,6 @@ package ej2;
 
 public class NegativeSecondException extends Exception {
 public String toString() {
-	return "No se puede colocar un digito menor a cero";
+	return "No se puede colocar un segundo menor a cero";
 }
 }
